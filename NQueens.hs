dad349e5991be20ff07eaf6e1114014eb9ffe1c4
Jennifer Jacobs <jejacobs@mines.edu>
2015-11-29 23:16:29 -0700

    adding finished probably code

--- NQueens.hs
+++ NQueens.hs
@@ -1,58 +1,49 @@
-
-
-{--nQueens size
-	|size == 2 = "No solution"
-	|size == 3 = "No solution"
-	|otherwise = length $ nQueensNumSoln size
-
-nQueensNumSoln size
-	|size == 2 = "No solution"
-	|size == 3 = "No solution"
-	|otherwise = helpQueens size
-	
-helpQueens size = [(1,2), (4,4)]
-	
--- wasn't sure how to declare this but something like this to store the locations of the queens like she proposed
---queenLoc [(Int, Int)]
-
-
-conflictRow board x xs = 
-	| 
-	|
-	where isConflict = sort xs
---}
- module NQueens where
+module NQueens where
 import Control.Monad
 import Data.List
- 
+import System.IO  
+import System.Directory  
 
-
--- given n, "queens n" solves the n-queens problem, returning a list of all the
--- safe arrangements. each solution is a list of the columns where the queens are
--- located for each row
-queens :: Int -> [[Int]]
-queens n = map fst $ foldM oneMoreQueen ([],[1..n]) [1..n]  where 
+calcSolution :: Int -> [[Int]]
+calcSolution n = map fst $ foldM addQueen ([],[1..n]) [1..n]  where 
  
-  -- foldM :: (Monad m) => (a -> b -> m a) -> a -> [b] -> m a
-  -- foldM folds (from left to right) in the list monad, which is convenient for 
-  -- "nondeterminstically" finding "all possible solutions" of something. the 
-  -- initial value [] corresponds to the only safe arrangement of queens in 0 rows
- 
-  -- given a safe arrangement y of queens in the first i rows, and a list of 
-  -- possible choices, "oneMoreQueen y _" returns a list of all the safe 
-  -- arrangements of queens in the first (i+1) rows along with remaining choices 
-  oneMoreQueen (y,d) _ = [(x:y, delete x d) | x <- d, safe x]  where
+  -- adds a queen to the board
+  addQueen (y,d) _ = [(x:y, delete x d) | x <- d, isSafe x]  where
  
     -- "safe x" tests whether a queen at column x is safe from previous queens
-    safe x = and [x /= c + n && x /= c - n | (n,c) <- zip [1..] y]
- 
--- prints what the board looks like for a solution; with an extra newline
-printSolution y = do
-     let n = length y
-     mapM_ (\x -> putStrLn [if z == x then 'Q' else 'X' | z <- [1..n]]) y
-     putStrLn ""
- 
--- prints all the solutions for 6 queens
-main = mapM_ printSolution $ queens 3
+    isSafe x = and [x /= c + n && x /= c - n | (n,c) <- zip [1..] y]
+    -- returns whether a queen is safe from other queens
 
+-- prints solutions
+printSol y = do
+  mapM_ (\x -> putStrLn [if z == x then 'Q' else 'X' | z <- [1..(length y)]]) y
+  putStrLn ""
+
+--prompts you for a number
+prompt x = do
+  putStrLn x
+  getLine
+
+nQueens = do
+	number <- prompt "Please input a number: "
+	let size = (read number::Int)
+	if size == 2
+		then putStrLn "No solution"
+	else  
+			(if size == 3
+			then putStrLn "No solution"
+			else 
+			 mapM_ printSol $ calcSolution size)
+
+
+nQueensNumSoln = do
+  number <- prompt "Please input a number: "
+  let size = (read number::Int)
+  if size == 2
+		then putStrLn "No solution"
+	else  
+			(if size == 3
+			then putStrLn "No solution"
+			else 
+			 putStrLn (show (length (calcSolution size))))
 
