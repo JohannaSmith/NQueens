15452ca7a507aed9c93134e6f97d31cf61efa6dd
Jennifer Jacobs <jejacobs@mines.edu>
2015-11-29 22:10:28 -0700

    added code to get solutions

--- NQueens.hs
+++ NQueens.hs
@@ -1,7 +1,6 @@
-module NQueens where
 
 
-nQueens size
+{--nQueens size
 	|size == 2 = "No solution"
 	|size == 3 = "No solution"
 	|otherwise = length $ nQueensNumSoln size
@@ -17,7 +16,43 @@ helpQueens size = [(1,2), (4,4)]
 --queenLoc [(Int, Int)]
 
 
---conflictRow board = 
---I was thinking something like this to see if a row was conflicted, but didn't actually start it
---	elem' y ys = foldr (\x acc -> if x == y then True else acc) False ys
+conflictRow board x xs = 
+	| 
+	|
+	where isConflict = sort xs
+--}
+ module NQueens where
+import Control.Monad
+import Data.List
+ 
+
+
+-- given n, "queens n" solves the n-queens problem, returning a list of all the
+-- safe arrangements. each solution is a list of the columns where the queens are
+-- located for each row
+queens :: Int -> [[Int]]
+queens n = map fst $ foldM oneMoreQueen ([],[1..n]) [1..n]  where 
+ 
+  -- foldM :: (Monad m) => (a -> b -> m a) -> a -> [b] -> m a
+  -- foldM folds (from left to right) in the list monad, which is convenient for 
+  -- "nondeterminstically" finding "all possible solutions" of something. the 
+  -- initial value [] corresponds to the only safe arrangement of queens in 0 rows
+ 
+  -- given a safe arrangement y of queens in the first i rows, and a list of 
+  -- possible choices, "oneMoreQueen y _" returns a list of all the safe 
+  -- arrangements of queens in the first (i+1) rows along with remaining choices 
+  oneMoreQueen (y,d) _ = [(x:y, delete x d) | x <- d, safe x]  where
+ 
+    -- "safe x" tests whether a queen at column x is safe from previous queens
+    safe x = and [x /= c + n && x /= c - n | (n,c) <- zip [1..] y]
+ 
+-- prints what the board looks like for a solution; with an extra newline
+printSolution y = do
+     let n = length y
+     mapM_ (\x -> putStrLn [if z == x then 'Q' else 'X' | z <- [1..n]]) y
+     putStrLn ""
+ 
+-- prints all the solutions for 6 queens
+main = mapM_ printSolution $ queens 3
+
 
